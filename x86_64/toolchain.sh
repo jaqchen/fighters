eda7373324614cc8f5c841d7a760ea60bacb0917
yejq <yejq.jiaqiang@gmail.com>
2026-09-26 10:07:49 +0800

    x86_64: remove unused files

--- x86_64/toolchain.sh
+++ x86_64/toolchain.sh
@@ -23,6 +23,6 @@ declare -r FTC_AR="${FTC_PREFIX}ar"
 declare -r FTC_RANLIB="${FTC_PREFIX}ranlib"
 declare -r FTC_LD="${FTC_PREFIX}ld"
 
-declare -r FTC_FLAGS="-Wall -fPIC -O2 -ggdb -D_GNU_SOURCE"
+declare -r FTC_FLAGS="-Wall -fPIC -O2 -ggdb -D_GNU_SOURCE -fno-omit-frame-pointer"
 declare -r FTC_CFLAGS="${FTC_FLAGS}"
 declare -r FTC_CXXFLAGS="${FTC_FLAGS}"
